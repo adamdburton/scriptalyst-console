2c1a21327768e4579fdc40e87253a2ab48f39bf4
Adam Burton <adam@burt0n.net>
2014-11-25 13:04:36 +0000

    Hmm

---
+++ Scriptalyst-Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MoonSharp.Interpreter;
+
+namespace ScriptalystConsole
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			UserData.RegisterType<ScriptalystConsole>();
+
+			Script script = new Script ();
+
+			DynValue scriptalystConsole = UserData.Create(new ScriptalystConsole());
+
+			script.Globals.Set("scriptalyst", scriptalystConsole);
+
+			script.RunFile ("scriptalyst.lua");
+		}
+	}
+}
